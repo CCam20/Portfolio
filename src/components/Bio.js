d7347e1cf0b37b0e87fd4952a2ee665262acb449
Colin Cameron <ccameron2020@gmail.com>
2023-01-19 22:20:11 +0000

    added links to git and linkedIn

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Bio = () => {
   return (
-    <div>Bio</div>
+    <div>Website under construction. Please check out my github(https://github.com/CCam20) and linkedIn(https://www.linkedin.com/in/colin-cameron-8903b7256/) in the meantime</div>
   )
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div>Website under construction. Please check out my github(https://github.com/CCam20) and linkedIn(https://www.linkedin.com/in/colin-cameron-8903b7256/) in the meantime</div>
+
   )
 }
 
