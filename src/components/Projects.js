9e52dc2860bc1d3c5c7ff87c8dc0150f8c318eab
Colin Cameron <ccameron2020@gmail.com>
2023-01-20 09:57:28 +0000

    typos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,9 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 const Projects = () => {
   return (
-    <div>Website under construction. Please check out my github <a href="https://github.com/CCam20" target="_blank">https://github.com/CCam20</a> and linkedIn <a href="https://www.linkedin.com/in/colin-cameron-8903b7256/" target="_blank">https://www.linkedin.com/in/colin-cameron-8903b7256/</a> in the meantime</div>
-
-
+    <div>Website under construction. Please check out my github <a href="https://github.com/CCam20" target="_blank">Github</a> and <a href="https://www.linkedin.com/in/colin-cameron-8903b7256/" target="_blank">linkedIn</a> in the meantime</div>
   )
 }
 
