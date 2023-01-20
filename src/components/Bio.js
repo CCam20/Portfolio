d92c507c25163fba9c09f9841e45c7cae47b85fb
Colin Cameron <ccameron2020@gmail.com>
2023-01-20 01:40:32 +0000

    removed links

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Bio = () => {
   return (
-    <div>Website under construction. Please check out my github <a href="https://github.com/CCam20" target="_blank">https://github.com/CCam20</a> and linkedIn <a href="https://www.linkedin.com/in/colin-cameron-8903b7256/" target="_blank">https://www.linkedin.com/in/colin-cameron-8903b7256/</a> in the meantime</div>
+    <div>Website under construction. Please check out my <a href="https://github.com/CCam20" target="_blank">github </a> and linkedIn <a href="https://www.linkedin.com/in/colin-cameron-8903b7256/" target="_blank">linkedIn</a> in the meantime</div>
   )
 }
 
