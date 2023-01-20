1cb3d11ba8120d30955ef4be474b3fba57fb581f
Colin Cameron <ccameron2020@gmail.com>
2023-01-20 01:12:26 +0000

    added links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 const Projects = () => {
   return (
-    <div>Website under construction. Please check out my github <a href='https://github.com/CCam20'>https://github.com/CCam20</a> and linkedIn <a href='https://www.linkedin.com/in/colin-cameron-8903b7256/'>https://www.linkedin.com/in/colin-cameron-8903b7256/</a> in the meantime</div>
+    <div>Website under construction. Please check out my github <a href="https://github.com/CCam20" target="_blank">https://github.com/CCam20</a> and linkedIn <a href="https://www.linkedin.com/in/colin-cameron-8903b7256/" target="_blank">https://www.linkedin.com/in/colin-cameron-8903b7256/</a> in the meantime</div>
 
 
   )
